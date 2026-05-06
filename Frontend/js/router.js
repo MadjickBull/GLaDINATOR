e4d591ca4800b9dc045dc9ba68c688c8b68041a9
nokzbbox <nunopferreira01@gmail.com>
2026-05-06 23:14:20 +0100

    add personality toggle to header

--- Frontend/js/router.js
+++ Frontend/js/router.js
@@ -1,7 +1,8 @@
 import routes from "../js/routes.js";
 
 export default function start() {
-  const path = window.location.pathname;
+  const path = window.location.pathname.replace("/Frontend/home.html", "/");
+  //const path = window.location.pathname;
   navigate(path, true);
   setAnchorEventListener();
   addEventListener("popstate", handlePopState);
